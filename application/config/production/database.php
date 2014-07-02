327efed899eb1b0c16e6390400d8ec965e429512
Jack Van <jack.van1988@gmail.com>
2014-07-02 18:07:51 +0800

    测试服务器mysql配置更新

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = '10244968';
 $db['default']['database'] = 'dotachuanqi';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'd_';
